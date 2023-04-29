241120ab6a59249b04e1b3a8c2724a6035d67332
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-04-29 14:08:44 +0900

    完成

--- Practice/sample/stopwatch/develop/index.js
+++ Practice/sample/stopwatch/develop/index.js
@@ -18,12 +18,29 @@ var elmReset = document.querySelector('#reset');
 ================================*/
 // アプリケーション初期化
 var onPageLoad = function () {
+    updateView();
 };
 // スタート処理
 var onStart = function () {
+    //停止中の場合
+    if (isRunning === false) {
+        //タイマー起動
+        startTimer();
+    }
+    //計測中の場合
+    else {
+        //タイマー停止
+        stopTimer();
+    }
 };
 // リセット処理
 var onReset = function () {
+    //タイマーの停止
+    stopTimer();
+    //タイマーをリセット
+    resetTimer();
+    //描画の更新
+    updateView();
 };
 /*==============================
 /イベントリスナーを設定する
@@ -37,4 +54,41 @@ elmReset.addEventListener('click', onReset);
 /*==============================
 //その他自作関数を定義する
 ================================*/
-// timeCount の値を分：秒　ミリ秒
+// timeCount の値を「分：秒　ミリ秒」形式に変換し、画面描画更新
+function updateView() {
+    // 最大表示時間を超えない制限
+    if (timeCount > 60 * 60 * 1000 - 1) {
+        timeCount = 60 * 60 * 1000 - 1; //59:59 99
+    }
+    // 「ミリ秒」を求める
+    var ms = (timeCount % 1000).toString().padStart(3, '0').slice(0, 2);
+    // 秒を求める
+    var ss = (Math.floor(timeCount / 1000) % 60).toString().padStart(2, '0');
+    //分を求める
+    var mm = Math.floor(timeCount / 1000 / 60).toString().padStart(2, '0');
+    //表示する文字列を編集
+    var count = mm + ':' + ss + ' <small>' + ms + '</small>';
+    // カウントの更新
+    elmCount.innerHTML = count;
+}
+;
+function startTimer() {
+    // 指定された時間ごとにカウントを更新
+    timerID = window.setInterval(function () {
+        //経過時間を加算
+        timeCount += 10;
+        //描画を更新
+        updateView();
+    }, 10);
+    //計測状態を「計測中」に変更
+    isRunning = true;
+}
+function stopTimer() {
+    //タイマーを停止
+    clearInterval(timerID);
+    //計測状態を「停止中」に変更
+    isRunning = false;
+}
+function resetTimer() {
+    timeCount = 0;
+}
--- Practice/sample/stopwatch/develop/index.ts
+++ Practice/sample/stopwatch/develop/index.ts
@@ -1,6 +1,9 @@
 /*==============================
 グローバル変数
 ================================*/
+
+
+
 //スタートからの経過時間
 let timeCount: number = 0;
 //計測状態（計測中：true、停止中:false)
@@ -25,16 +28,33 @@ const elmReset: HTMLElement = document.querySelector('#reset')!;
 
 // アプリケーション初期化
 const onPageLoad = () => {
-    
+    updateView()
 };
 
 // スタート処理
 const onStart = () => {
-    
+    //停止中の場合
+    if (isRunning === false) {
+        //タイマー起動
+        startTimer();
+
+    }
+    //計測中の場合
+    else {
+        //タイマー停止
+        stopTimer();
+
+    }
 };
 
 // リセット処理
 const onReset = () => {
+    //タイマーの停止
+    stopTimer();
+    //タイマーをリセット
+    resetTimer();
+    //描画の更新
+    updateView();
 
 };
 
@@ -57,4 +77,48 @@ elmReset.addEventListener('click', onReset);
 //その他自作関数を定義する
 ================================*/
 
-// timeCount の値を分：秒　ミリ秒
+// timeCount の値を「分：秒　ミリ秒」形式に変換し、画面描画更新
+function updateView() {
+    // 最大表示時間を超えない制限
+    if (timeCount > 60 * 60 * 1000 - 1) {
+        timeCount = 60 * 60 * 1000 -1 //59:59 99
+    }
+
+    // 「ミリ秒」を求める
+    const ms: string = (timeCount % 1000).toString().padStart(3, '0').slice(0, 2);
+    
+    // 秒を求める
+    const ss: string = (Math.floor(timeCount / 1000) % 60).toString().padStart(2, '0');
+    
+    //分を求める
+    const mm: string = Math.floor(timeCount / 1000 / 60).toString().padStart(2, '0');
+
+    //表示する文字列を編集
+    const count: string = mm + ':' + ss + ' <small>' + ms + '</small>';
+    // カウントの更新
+    elmCount.innerHTML = count;
+};
+
+function startTimer() {
+    // 指定された時間ごとにカウントを更新
+    timerID = window.setInterval(() => {
+        //経過時間を加算
+        timeCount += 10;
+        //描画を更新
+        updateView();
+    }, 10);
+
+    //計測状態を「計測中」に変更
+    isRunning = true;
+}
+
+function stopTimer() {
+    //タイマーを停止
+    clearInterval(timerID);
+    //計測状態を「停止中」に変更
+    isRunning = false;
+}
+
+function resetTimer() {
+    timeCount = 0
+}
