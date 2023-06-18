ccdcdcda9261cbca80de84d866233faf793d01a4
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-18 11:23:50 +0900

    インポート方法バージョンアップ

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,7 +1,7 @@
 //useReducerをimport
 import React, {useReducer} from 'react'
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 //counterの初期値を0に設定
 const initialState = 0
